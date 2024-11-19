f50002491c9128d7934f24655f05caddcab1cec6
Jarvis Choy <choyja2@oocl.com>
2024-11-19 15:41:16 +0800

    test: given facing North, when turn right, should face E

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -27,4 +27,17 @@ public class MarsRoverTest {
         assertEquals("0:0:W", report);
     }
 
+    @Test
+    public void should_face_east_when_orient_north_and_turn_right(){
+        // Given
+        MarsRover rover = new MarsRover();
+
+        // When
+        String response = rover.execCommand("R");
+        String report = rover.getStatus();
+
+        // Then
+        assertEquals("0:0:E", report);
+    }
+
 }
