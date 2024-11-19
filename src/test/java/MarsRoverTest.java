2d05b0e4bcac1c46910709eee75940be48b66377
Jarvis Choy <choyja2@oocl.com>
2024-11-19 15:33:08 +0800

    test: given facing North, when turn left, should face West

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -14,4 +14,17 @@ public class MarsRoverTest {
         assertNotNull(rover);
     }
 
+    @Test
+    public void should_face_west_when_orient_north_and_turn_left(){
+        // Given
+        MarsRover rover = new MarsRover();
+
+        // When
+        String response = rover.execCommand("L");
+        String report = rover.getStatus();
+
+        // Then
+        assertEquals("0:0:W", report);
+    }
+
 }
